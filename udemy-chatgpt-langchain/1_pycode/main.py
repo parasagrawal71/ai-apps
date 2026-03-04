a900128b57a38ab96f2b62f9d19eb004bf096e06
Paras Agrawal <parasagrawal71@gmail.com>
2026-03-04 22:58:37 +0530

    Add a basic code to query to OpenAI LLM

--- udemy-chatgpt-langchain/1_pycode/main.py
+++ udemy-chatgpt-langchain/1_pycode/main.py
@@ -1,1 +1,12 @@
-print('hi there')
+from langchain_openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(api_key=API_KEY)
+
+result = llm("write a very very short poem")
+print(result)
--- udemy-chatgpt-langchain/README.md
+++ udemy-chatgpt-langchain/README.md
@@ -1,2 +1,2 @@
-# ChatGPT and LangChain: The Complete Developer's Masterclass
+# ChatGPT and LangChain: The Complete Developer's Masterclass (Python)
 https://www.udemy.com/course/chatgpt-and-langchain-the-complete-developers-masterclass/
