1e125da74b65b26c5c350be600a346e4257a310b
Paras Agrawal <parasagrawal71@gmail.com>
2026-03-04 23:45:12 +0530

    Connect chains together

--- udemy-chatgpt-langchain/1_pycode/main.py
+++ udemy-chatgpt-langchain/1_pycode/main.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
 import argparse
+from pprintpp import pprint
 # from langchain_openai import OpenAI
 from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 
 # -- Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -19,19 +20,36 @@ load_dotenv()
 # API_KEY = os.getenv("OPENAI_API_KEY")
 API_KEY = os.getenv("GROQ_API_KEY")
 
+# -- Initialize LLM
 # llm = OpenAI(api_key=API_KEY)
 llm = ChatGroq(
     groq_api_key=API_KEY,
     model_name="llama-3.1-8b-instant"
 )
 
+# -- Templates
 code_prompt = PromptTemplate(
     template="write a very short {language} function that will {task}",
     input_variables=["language", "task"],
 )
+testcase_prompt = PromptTemplate(
+    template="write a test case for the following {language} code:\n {code}",
+    input_variables=["language", "code"],
+)
 
-code_chain = LLMChain(llm=llm, prompt=code_prompt)
+# -- Chains
+code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
+testcase_chain = LLMChain(llm=llm, prompt=testcase_prompt, output_key="testcase")
 
-result = code_chain({"language": args.language, "task": args.task})
-# print(result)
-print(result["text"])
+# --
+chain = SequentialChain(
+    chains=[code_chain, testcase_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "testcase"],
+)
+
+result = chain({"language": args.language, "task": args.task})
+pprint(result)
+# print(result["text"]) # text is 'code' now
+# pprint(result["code"])
+# pprint(result["testcase"])
