b6c90be6c6af1d99094032829a751e4adf57f4ee
Paras Agrawal <parasagrawal71@gmail.com>
2026-03-04 23:27:13 +0530

    Add prompt and chain using Groq LLM (free)

--- udemy-chatgpt-langchain/1_pycode/main.py
+++ udemy-chatgpt-langchain/1_pycode/main.py
@@ -1,12 +1,37 @@
-from langchain_openai import OpenAI
 import os
 from dotenv import load_dotenv
+import argparse
+# from langchain_openai import OpenAI
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
+# -- Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="add two numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
+
+# -- Load environment variables
 load_dotenv()
 
-API_KEY = os.getenv("OPENAI_API_KEY")
+# -- Set API key
+# API_KEY = os.getenv("OPENAI_API_KEY")
+API_KEY = os.getenv("GROQ_API_KEY")
 
-llm = OpenAI(api_key=API_KEY)
+# llm = OpenAI(api_key=API_KEY)
+llm = ChatGroq(
+    groq_api_key=API_KEY,
+    model_name="llama-3.1-8b-instant"
+)
 
-result = llm("write a very very short poem")
-print(result)
+code_prompt = PromptTemplate(
+    template="write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
+)
+
+code_chain = LLMChain(llm=llm, prompt=code_prompt)
+
+result = code_chain({"language": args.language, "task": args.task})
+# print(result)
+print(result["text"])
