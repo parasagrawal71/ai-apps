1474e846bc14f85eee77858a46ba531bdb341c89
Paras Agrawal <parasagrawal71@gmail.com>
2026-03-24 18:28:23 +0530

    Add build_retriever

--- udemy-chatgpt-langchain/5_pdf/app/chat/chat.py
+++ udemy-chatgpt-langchain/5_pdf/app/chat/chat.py
@@ -1,4 +1,5 @@
 from app.chat.models import ChatArgs
+from app.chat.vector_stores.pinecone import build_retriever
 
 
 def build_chat(chat_args: ChatArgs):
@@ -12,5 +13,7 @@ def build_chat(chat_args: ChatArgs):
 
         chain = build_chat(chat_args)
     """
+
+    retriever = build_retriever(chat_args)
 
     pass
--- udemy-chatgpt-langchain/5_pdf/app/chat/vector_stores/pinecone.py
+++ udemy-chatgpt-langchain/5_pdf/app/chat/vector_stores/pinecone.py
@@ -17,3 +17,11 @@ vector_store = Pinecone.from_existing_index(
     index_name=os.getenv("PINECONE_INDEX_NAME"),
     embedding=embeddings,
 )
+
+def build_retriever(chat_args):
+    search_kwargs = {
+        "filter": {
+            "pdf_id": chat_args.pdf_id,
+        }
+    }
+    return vector_store.as_retriever(search_kwargs=search_kwargs)
